0c3975fce8bec6ea9dd838791331e15ccddadfb4
lorenzo <lorenzo>
2022-05-10 18:16:30 +0200

    GG-6:
    - Adding dark mode
    - Improved Layout
    - Adding new themes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'gsped-widzard';
+  themes = {
+    navbar: [
+      "bg-gradient-to-br from-purple-600 to-blue-500",
+      "bg-gradient-to-r from-blue-500",
+      "bg-gradient-to-br from-green-600 to-green-300",
+      "bg-gradient-to-br from-cyan-200 to-rose-500",
+      "bg-gradient-to-br from-purple-300 to-slate-200",
+      "bg-gradient-to-r from-indigo-500 via-cyan-400 to-green-500 dark:from-purple-900 dark:to-purple-700",
+    ],
+    buttons: [
+      "bg-gradient-to-br from-purple-300 to-slate-200",
+      "bg-gradient-to-r from-blue-500",
+      "bg-gradient-to-r from-indigo-500 via-cyan-400 to-green-500 dark:from-purple-900 dark:to-purple-700",
+      "bg-gradient-to-br from-green-600 to-green-300",
+      "bg-gradient-to-br from-cyan-200 to-rose-500",
+      "bg-gradient-to-br from-purple-600 to-blue-500",
+    ],
+  };
+
+  navbarTheme = this.themes.navbar[0];
+  buttonsTheme = this.themes.buttons[0];
+
+  setTheme(type: number) {
+    this.navbarTheme = this.themes.navbar[type];
+    this.buttonsTheme = this.themes.buttons[type];
+  }
 }
--- src/app/widzard/widzard.component.ts
+++ src/app/widzard/widzard.component.ts
@@ -1,89 +1,91 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-widzard',
-  templateUrl: './widzard.component.html',
-  styleUrls: ['./widzard.component.css'],
+  selector: "app-widzard",
+  templateUrl: "./widzard.component.html",
+  styleUrls: ["./widzard.component.css"],
 })
 export class WidzardComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
 
+  @Input() theme = "";
+
   activeStep: number = 0;
 
   widzard = [
     {
-      type: 'Reso Facile',
+      type: "Reso Facile",
       content: [
         {
-          step: 'Modulo Ricerca Spedizione',
+          step: "Modulo Ricerca Spedizione",
           fields: [
-            { title: 'ID spedizione', type: 'text' },
+            { title: "ID spedizione", type: "text" },
             {
-              title: 'Riferimento alfanumerico',
-              type: 'text',
+              title: "Riferimento alfanumerico",
+              type: "text",
             },
-            { title: 'Email Destinatario', type: 'email' },
+            { title: "Email Destinatario", type: "email" },
             {
-              title: 'Numero di telefono del destinatario',
-              type: 'number',
+              title: "Numero di telefono del destinatario",
+              type: "number",
             },
           ],
         },
         {
-          step: 'Modulo RITIRO (ritiro o DropOff)',
+          step: "Modulo RITIRO (ritiro o DropOff)",
           fields: [
-            { title: 'Dati Mittente', type: 'text' },
+            { title: "Dati Mittente", type: "text" },
             {
-              title: 'Data e ora Pickup',
-              type: 'date',
+              title: "Data e ora Pickup",
+              type: "date",
             },
-            { title: 'Client ID', type: 'text' },
+            { title: "Client ID", type: "text" },
           ],
         },
         {
-          step: 'Modulo rating spedizione',
-          fields: [{ title: 'V/Documentazione', type: 'text' }],
+          step: "Modulo rating spedizione",
+          fields: [{ title: "V/Documentazione", type: "text" }],
         },
         {
-          step: 'Modulo Pagamento',
+          step: "Modulo Pagamento",
           fields: [
-            { title: 'Provider di pagamento', type: 'text' },
+            { title: "Provider di pagamento", type: "text" },
             {
-              title: 'Client ID',
-              type: 'text',
+              title: "Client ID",
+              type: "text",
             },
           ],
         },
         {
-          step: 'Modulo Stampa AWB',
+          step: "Modulo Stampa AWB",
           fields: [
-            { title: 'ID Transizione', type: 'text' },
+            { title: "ID Transizione", type: "text" },
             {
-              title: 'ID Spedizione',
-              type: 'text',
+              title: "ID Spedizione",
+              type: "text",
             },
-            { title: 'Provider pagamento', type: 'text' },
+            { title: "Provider pagamento", type: "text" },
           ],
         },
       ],
     },
     {
-      type: 'altro',
+      type: "altro",
       content: [
         {
-          step: 'Modulo Ricerca Spedizione',
+          step: "Modulo Ricerca Spedizione",
           fields: [
-            { title: 'ID spedizione', type: 'ID' },
+            { title: "ID spedizione", type: "ID" },
             {
-              title: 'Riferimento alfanumerico',
-              type: 'Riferimento Alfanumerico',
+              title: "Riferimento alfanumerico",
+              type: "Riferimento Alfanumerico",
             },
-            { title: 'Email Destinatario', type: 'Email' },
+            { title: "Email Destinatario", type: "Email" },
             {
-              title: 'Numero di telefono del destinatario',
-              type: 'numero di telefono',
+              title: "Numero di telefono del destinatario",
+              type: "numero di telefono",
             },
           ],
         },

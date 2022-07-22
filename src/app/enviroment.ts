7dc0853aa7738454e55b5a1ea0bdc160b69d98ce
lorenzo <lorenzo>
2022-07-22 16:34:50 +0200

    new features:
    - setting up fatturea dhl form
    - fixed current_url enviroment constant

--- src/app/enviroment.ts
+++ src/app/enviroment.ts
@@ -2,7 +2,7 @@ export const environment = {
   PRODUCTION: false,
   API_URL: "https://api.gsped.it/",
   // development
-  CURRENT_URL: "http://localhost:4200/",
+  CURRENT_URL: window.document.location.origin,
   // production
   // CURRENT_URL: "http://localhost:4200/",
   FLOW: "resi",
--- src/app/moduli/fattura-dhl/fattura-dhl.component.ts
+++ src/app/moduli/fattura-dhl/fattura-dhl.component.ts
@@ -15,64 +15,128 @@ export class FatturaDHLComponent implements OnInit {
     private store: StoreService,
     private status: StatusService
   ) {
+    this.selected = "privato";
     this.formInvoice = fb.group({
-      sender_name: ["", Validators.required],
-      sender_city: ["", Validators.required],
-      sender_contact: ["", Validators.required],
-      sender_cap: ["", Validators.required],
-      sender_prov: ["", Validators.required],
-      sender_country_code: ["", Validators.required],
-      sender_email: ["", Validators.required],
-      sender_phone: ["", Validators.required],
-      sender_addr: ["", Validators.required],
+      codice_fiscale: ["", Validators.required],
+      pec: [""],
+      sdi: ["", Validators.required],
+      // nome: ["", Validators.required],
+      // cognome: ["", Validators.required],
+      // societa: [""],
+      // indirizzo: ["", Validators.required],
+      // nazione: ["", Validators.required],
+      // cap: ["", Validators.required],
+      // citta: ["", Validators.required],
+      // email: ["", Validators.required],
     });
   }
 
   ngOnInit(): void {}
 
-  invoiceModules: any;
+  invoiceModules: any = [
+    {
+      value: "codice_fiscale",
+      label: "codice fiscale",
+      type: "text",
+      required: true,
+    },
+    { value: "pec", label: "PEC", type: "email", required: false },
+    { value: "sdi", label: "CFE/SDI", type: "text", required: true },
+  ];
   formInvoice: FormGroup;
+  selected: string;
+  otherModule: string =
+    "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 w-1/3 py-4 px-1 text-center border-b-2 font-medium text-sm uppercase cursor-pointer flex-nowrap";
+  currentModule: any =
+    "border-indigo-500 text-indigo-600 w-1/3 py-4 px-1 text-center border-b-2 font-medium text-sm uppercase cursor-pointer flex-nowrap";
+
+  handleSetInvoiceModules(type:string) {
+    if(confirm("Sei sucuro di cambaire modulo? i dati fin'ora inseriti andranno persi")) {
+      this.setInvoiceModules(type);
+    }
+  }
 
   setInvoiceModules(type: string) {
+    this.selected = type;
+    this.formInvoice = this.fb.group({});
     switch (type) {
       case "privato":
+        this.formInvoice = this.fb.group({
+          codice_fiscale: ["", Validators.required],
+          pec: [""],
+          sdi: ["", Validators.required],
+        });
         this.invoiceModules = [
-          { label: "codice fiscale", type: "text", required: true },
-          { label: "PEC", type: "email", required: false },
-          { label: "CFE/SDI", type: "text", required: true },
+          {
+            value: "codice_fiscale",
+            label: "codice fiscale",
+            type: "text",
+            required: true,
+          },
+          {
+            value: "pec",
+            label: "PEC",
+            type: "email",
+            required: false,
+          },
+          { value: "sdi", label: "CFE/SDI", type: "text", required: true },
         ];
         break;
       case "piva":
+        this.formInvoice = this.fb.group({
+          codice_fiscale: ["", Validators.required],
+          pec: [""],
+          sdi: ["", Validators.required],
+        });
         this.invoiceModules = [
-          { label: "partita iva", type: "text", required: true },
-          { label: "PEC", type: "email", required: false },
-          { label: "CFE/SDI", type: "text", required: true },
+          {
+            value: "codice_fiscale",
+            label: "partita iva",
+            type: "text",
+            required: true,
+          },
+          { value: "pec", label: "PEC", type: "email", required: false },
+          { value: "sdi", label: "CFE/SDI", type: "text", required: true },
         ];
         break;
       case "estero":
+        this.formInvoice = this.fb.group({
+          nome: ["", Validators.required],
+          cognome: ["", Validators.required],
+          societa: [""],
+          indirizzo: ["", Validators.required],
+          nazione: ["", Validators.required],
+          cap: ["", Validators.required],
+          citta: ["", Validators.required],
+          email: ["", Validators.required],
+        });
         this.invoiceModules = [
-          { label: "nome", type: "email", required: true },
-          { label: "cognome", type: "text", required: true },
-          { label: "società", type: "text", required: false },
-          { label: "indirizzo", type: "text", required: true },
-          { label: "country", type: "text", required: true },
-          { label: "CAP", type: "text", required: true },
-          { label: "Città", type: "text", required: true },
-          { label: "e-mail", type: "email", required: true },
+          { value: "nome", label: "nome", type: "email", required: true },
+          { value: "cognome", label: "cognome", type: "text", required: true },
+          { value: "societa", label: "società", type: "text", required: false },
+          {
+            value: "indirizzo",
+            label: "indirizzo",
+            type: "text",
+            required: true,
+          },
+          { value: "nazione", label: "nazione", type: "text", required: true },
+          { value: "cap", label: "CAP", type: "text", required: true },
+          { value: "citta", label: "Città", type: "text", required: true },
+          { value: "email", label: "e-mail", type: "email", required: true },
         ];
         break;
     }
   }
 
   nextStep() {
-    this.store.payloadShipment.fatturaDHL = [
-      {
-        cap: 15121,
-      },
-    ];
-    console.log(this.store.payloadShipment);
     // TODO settare il form
-    if (this.formInvoice.valid || true) {
+    console.log(this.formInvoice.value);
+    console.log(this.formInvoice.valid);
+    if (this.formInvoice.valid) {
+      this.store.payloadShipment.fattura_dhl = [
+        this.formInvoice.value
+      ];
       this.store.invoice = this.formInvoice.value;
       this.status
         .handleShipment(this.store.payloadShipment)

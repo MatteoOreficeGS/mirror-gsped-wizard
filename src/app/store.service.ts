5df49f1ceedb2347b6ce0ec3699a98d509ebc24b
ISoreca <i.soreca@bluecube.it>
2022-08-04 14:38:41 +0200

    Changed and centered the loader

--- src/app/store.service.ts
+++ src/app/store.service.ts
@@ -17,24 +17,24 @@ export class StoreService {
   modules?: any;
   sender: any = {
     //RIMUOVERE
-    // sender_name: "Mario Rossi Verdi",
-    // sender_city: "Milan",
-    // sender_cap: "20121",
-    // sender_prov: "MI",
-    // sender_country_code: "IT",
-    // sender_email: "mail@gmail.com",
-    // sender_phone: "3343343344",
-    // sender_addr: "Via Dante 23",
+    sender_name: "Mario Rossi Verdi",
+    sender_city: "Milan",
+    sender_cap: "20121",
+    sender_prov: "MI",
+    sender_country_code: "IT",
+    sender_email: "mail@gmail.com",
+    sender_phone: "3343343344",
+    sender_addr: "Via Dante 23",
   };
   recipient: any = {
-    // rcpt_name: "Mario Rossi Verdi",
-    // rcpt_city: "Verona",
-    // rcpt_cap: "37138",
-    // rcpt_prov: "VR",
-    // rcpt_country_code: "IT",
-    // rcpt_email: "mail2@gmail.com",
-    // rcpt_phone: "3343343346",
-    // rcpt_addr: "Via Pittagora 1",
+    rcpt_name: "Mario Rossi Verdi",
+    rcpt_city: "Verona",
+    rcpt_cap: "37138",
+    rcpt_prov: "VR",
+    rcpt_country_code: "IT",
+    rcpt_email: "mail2@gmail.com",
+    rcpt_phone: "3343343346",
+    rcpt_addr: "Via Pittagora 1",
   };
   outwardShipment: any = {};
   returnShipment: any = {};

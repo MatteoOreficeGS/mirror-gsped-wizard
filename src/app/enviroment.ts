85b8c7c86e1c8885eb3b1f08505a6b1e9488e11c
Fabio Alessio <andrei@gsped.com>
2023-10-13 08:15:07 +0200

    modifica indirizzo api

--- src/app/enviroment.ts
+++ src/app/enviroment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   PRODUCTION: false,
-  API_URL: "http://apistaging.aws.gsped.it/",
+  API_URL: "https://api.gsped.it/",
   CURRENT_URL: window.document.location.origin,
   FLOW: "resi",
   DHL_API_KEY: "Vg9TTemSNaXE4G6PwAfhbwCeK0x0vswz",
